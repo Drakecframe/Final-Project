c55346e3542412c71df3cc3a2feef1248491ca3c
Drakecframe <nelphyrr20@gmail.com>
2025-08-25 19:08:07 -0700

    Update Final-Project.cpp

--- Final-Project.cpp
+++ Final-Project.cpp
@@ -1,26 +1,17 @@
-#include <math.h>
-#include <iomanip>
-#include <fstream>
-#include <iostream> // For outputting to console
-#include <string>   // For reading lines into a string
+    #include <iostream>
+    #include <fstream>
+    #include <string>
+    #include <vector>
 
 using namespace std;
 
 int main()
 {
-    //const string path = R"(C:\Users\Nelphy\Documents\WordTest.xlsx)";
-    //ifstream MyFile("C:\Users\Nelphy\Documents\WordTest.xlsx");
-  
-    ifstream MyFile;
-    MyFile.open("C:\\Users\\Nelphy\\Documents\\WordTest.xlsx");
+    ifstream My_File("//usr//share//dict//words");
+    My_File.open();
 
-    if (MyFile.is_open())
-    {
-        cout << "The file is open" << endl;
-    }
-    if (!MyFile.is_open())
-    {  
-        cout << "The file is closed" << endl;    
-    }
+
+    vector<string> words;
+
     return 0;
 }
