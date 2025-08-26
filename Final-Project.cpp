9e4bcbc721bb7fc3bf71c14d818e4b38a3cbea8e
Drakecframe <nelphyrr20@gmail.com>
2025-08-25 20:24:10 -0700

    Update Final-Project.cpp

--- Final-Project.cpp
+++ Final-Project.cpp
@@ -1,17 +1,36 @@
-    #include <iostream>
-    #include <fstream>
-    #include <string>
-    #include <vector>
+/* ---------------------------------------------------------------------------
+** I attest that this following code represents my own work and is subject to
+** the plagiarism policy found in the course syllabus.
+** 
+** Class: 	CSC 242
+** Assignment: 	
+** File: 	source.cpp
+** Description: 
+**
+** Author: 	Group not F
+** Date: 	8/25/2025
+** -------------------------------------------------------------------------*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    ifstream My_File("//usr//share//dict//words");
-    My_File.open();
-
+    ifstream My_File("C:\\Users\\Nelphy\\Desktop\\words");//This will get the file and open it.
+    
+    if (My_File.is_open())//This will check if the file is not open.
+    {
+       cerr << "Error: Could not open the file" << endl;//This will output the error.
+       return 1;
+    }
 
     vector<string> words;
+
+
 
     return 0;
 }
