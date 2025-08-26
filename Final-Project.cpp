4448b1762b68926651fb1a7e7276905425068f4b
Drakecframe <nelphyrr20@gmail.com>
2025-08-26 00:26:59 -0700

    Update Final-Project.cpp

--- Final-Project.cpp
+++ Final-Project.cpp
@@ -22,15 +22,45 @@ int main()
 {
     ifstream My_File("C:\\Users\\Nelphy\\Desktop\\words");//This will get the file and open it.
     
-    if (My_File.is_open())//This will check if the file is not open.
+    if (!My_File.is_open())//This will check if the file is not open(Just to be safe)
     {
        cerr << "Error: Could not open the file" << endl;//This will output the error.
        return 1;
     }
 
+    //This will store the words from the file into the vector.
     vector<string> words;
+    string read_lines;
+    while (getline(My_File, read_lines))
+    {
+        words.push_back(read_lines);
+    }
+    
+    My_File.close();
 
-
-
+    //User Input Word
+    string user_w;
+    cout << "Enter your word here: "; cin >> user_w;
+    
+    //Checking words in the vector
+    bool wordfound = false;
+    for (const string& word : words)
+    {
+        if (word == user_w)
+        {
+            wordfound = true;
+        }  
+        break; 
+    }
+    
+    //Check if the word is on the file and print the word
+    if (found)
+    {
+        cout << user_w << "is in the file!" << endl;
+    }
+    else 
+    {
+        cout << user_w << " is not in the file!" << endl;
+    }
     return 0;
 }
